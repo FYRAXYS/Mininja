f3519a5211e5691c1e8318476a04fb9992e0188a
FYRAXYS <fyraxys2006@gmail.com>
2025-06-15 11:36:26 +0200

    Tutorial popup
    I added a little popup at the start of the first level in order to guide the players with the differents inputs.

---
+++ docs/scripts/text_tuto_1.gd
@@ -0,0 +1,12 @@
+extends Control
+
+@onready var progress_bar = $MarginContainer/VBoxContainer/ProgressBar
+@onready var popup = $"."
+var time_left = 100
+
+func _process(delta: float) -> void:
+	progress_bar.value = time_left
+	time_left -= 0.3
+	
+	if progress_bar.value <= 0 :
+		popup.hide()
