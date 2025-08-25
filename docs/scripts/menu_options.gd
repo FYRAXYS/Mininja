435e5e5f398fe856b56fdc4928346fcf91fc3492
FYRAXYS <fyraxys2006@gmail.com>
2025-08-25 20:20:55 +0200

    Arrow adjustements
    added an option to turn on & off the arrow pointing to the end of the level.

--- docs/scripts/menu_options.gd
+++ docs/scripts/menu_options.gd
@@ -1,12 +1,16 @@
 extends Control
 
 var reset_level_path = "res://scenes/levels/lvl_1.tscn"
+@onready var arrow_button = $MarginContainer/VBoxContainer/HBoxArrow/Arrow
+
+func _ready() -> void:
+	arrow_button.set_pressed_no_signal(SaveManager.data["arrow"])
 
 func _on_reset_pressed() -> void:
-	var file = FileAccess.open("res://ressources/saves/currrent_level.save", FileAccess.WRITE)
-	file.store_var(reset_level_path) 
-	file.close()
+	SaveManager.reset_data()
 
+func _on_arrow_pressed() -> void:
+	SaveManager.update_arrow(!SaveManager.data["arrow"])
 
 func _on_exit_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/menus/menu.tscn")
